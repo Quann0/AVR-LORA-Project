102771f5ab2a2696d3eb225ce7c4e5a4b2bcf570
Quan <mas0987169143@gmail.com>
2022-05-18 12:49:53 +0700

    Update_v1.8

---
+++ firmware/Uart/Uart/main.c
@@ -0,0 +1,205 @@
+/*
+ * Uart.c
+ *
+ * Created: 4/27/2022 8:01:59 PM
+ * Author : QUAN
+ */ 
+
+
+#define F_CPU 8000000UL
+#include <stdio.h>
+#include <avr/io.h>
+//#include <stdio.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <string.h>
+#include <stdlib.h>
+
+#ifndef DHT11_H_
+#define DHT11_H_
+
+#define DHT11_ERROR 255
+#define DHT11_DDR DDRF
+#define DHT11_PORT PORTF
+#define DHT11_PIN PINF
+#define DHT11_INPUTPIN PF1
+
+#endif /* DHT11_H_ */
+
+void dht11_getdata(uint8_t num, uint8_t *data);
+uint8_t getdata(uint8_t select);
+char dataSend[4];
+
+void uart_char_tx(unsigned char chr) 
+{
+	while (bit_is_clear(UCSR0A,UDRE0)) { };
+	UDR0=chr;
+}
+void uart1_char_tx(char chr)
+{
+	while (bit_is_clear(UCSR1A,UDRE1)) { };
+	UDR1=chr;
+}
+void gui_1_chuoi_dulieu( char a[4])
+{
+	for(int i=0;i<strlen(a);i++)
+	{
+		uart1_char_tx(a[i]);
+	}
+}
+
+int read_adc(unsigned int adc_channel) //adc_channel l?u tham s? kênh ADC c?n ??c.
+{
+	ADMUX &= 0xf0;
+	ADMUX |= adc_channel; //Ch?n kênh ADC.
+	ADCSRA |=(1<<ADSC); //Cho phép b?t ??u quá tr?nh chuy?n ??i ADC: l?y giá tr? ?i?n áp vào (Vin) trên kênh ?? ch?n, sau ?ó th?c hi?n chuy?n ??i ADC theo công th?c:
+	while(bit_is_clear(ADCSRA,ADIF)) //trong khi th?c hi?n chuy?n ??i ADC (bit ADIF = 0).
+	{
+		; //Ch? trong quá tr?nh chuy?n ??i ADC, sau khi chuy?n ??i xong th? bit ADIF = 1.
+	}
+	_delay_ms(50);
+	//Ho?c có th? s? d?ng l?nh loop nh? sau ?? thay th? cho l?nh while bên trên:
+	//	loop_until_bit_is_set(ADCSRA,ADIF);
+	return ADCW; //Giá tr? chuy?n ??i ???c l?u vào thanh ghi ADCW 16 bit.
+}
+void led7seg(uint16_t ADC_val) {
+	uint16_t nghin,tram,chuc,donvi;
+	donvi = ADC_val % 10; //L?y giá tr? ADC chia cho 10 l?y ph?n d? gán vào bi?n donvi.
+	ADC_val /= 10; //L?y giá tr? ADC chia cho 10 l?y ph?n nguyên gán vào bi?n ADC_val
+	chuc = ADC_val % 10;
+	ADC_val /= 10;
+	tram = ADC_val % 10;
+	ADC_val /= 10;
+	nghin = ADC_val;
+	PORTA = nghin|(1<<PA4);//(1<<PE4):c?p ngu?n cho led hàng ngh?n sau ?ó OR v?i nghin.
+	_delay_ms(1);
+	PORTA = tram|(1<<PA5); //(1<<PE5):c?p ngu?n cho led hàng tr?m sau ?ó OR v?i tram.
+	_delay_ms(1);
+	PORTA = chuc|(1<<PA6);//(1<<PE6):c?p ngu?n cho led hàng ch?c sau ?ó OR v?i chuc.
+	_delay_ms(1);
+	PORTA = donvi|(1<<PA7);//(1<<PE7):c?p ngu?n cho led hàng ??n v? sau ?ó OR v?i donvi.
+	_delay_ms(1);
+}
+volatile unsigned char u_data;
+int main(void){
+
+	//Baudrate 9600, t?n s? f=8MHz
+	DDRA = 0xFF;
+	DDRB = 0xff;
+	UBRR0H=0;
+	UBRR0L=51;
+	UBRR1H=0;
+	UBRR1L=51;
+	UCSR0A=0x00;
+	UCSR0C =(1<<UCSZ01)|(1<<UCSZ00);
+	UCSR0B =(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0); //cho phép truy?n nh?n d? li?u và cho phép ng?t nh?n
+	UCSR1A=0x00;
+	UCSR1C =(1<<UCSZ11)|(1<<UCSZ10);
+	UCSR1B =(1<<RXEN1)|(1<<TXEN1)|(1<<RXCIE1);
+//	ADMUX |= (1<<REFS0); //Ch?n ?i?n áp tham chi?u AVCC
+//	ADCSRA |=(1<<ADEN) | (1<<ADPS2) | (1<<ADPS1)|(1<<ADPS0);//Cho phép ADC và ch?n h? s? chia xung nh?p cho ADC là 32.
+	sei(); //cho phép ng?t toàn c?c (bit I
+    /* Replace with your application code */
+	uint8_t datatemp = 0;
+	uint8_t dataHumi = 0;
+	uint16_t DataSum = 0;
+//	char buf[40] = {0,};
+    while (1) 
+    {
+		dht11_getdata(0, &datatemp);
+		dht11_getdata(1, &dataHumi);
+		DataSum=datatemp*100+dataHumi;
+		led7seg(DataSum);
+		//dataSend[0] = datatemp;
+		//dataSend[1] = dataHumi;
+		itoa(DataSum,dataSend,10);
+		gui_1_chuoi_dulieu(dataSend);
+		
+    }
+}
+
+ISR(USART0_RX_vect) { //hàm ph?c v? ng?t nh?n c?a UART0 thay cho hàm ISR(SIG_UART0_RECV)
+	u_data=UDR0;
+	if(u_data =='2')
+	{
+		PORTB |= (1<<PB0);
+	}
+	else if(u_data =='1')
+	{
+		PORTB &= ~(1<<PB0);
+	}
+	uart_char_tx(u_data);
+}
+
+/* get data from dht11 */
+uint8_t getdata(uint8_t select) {
+	uint8_t bits[5];
+	uint8_t i,j = 0;
+	
+	memset(bits, 0, sizeof(bits));
+	
+	//reset port
+	DHT11_DDR |= (1<<DHT11_INPUTPIN); //output
+	DHT11_PORT |= (1<<DHT11_INPUTPIN); //high
+	_delay_ms(100);
+	
+	//send request
+	DHT11_PORT &= ~(1<<DHT11_INPUTPIN); //low
+	_delay_ms(18);
+	//-- MCU pulls up voltage and waits for DHT response (20-40us)
+	DHT11_PORT |= (1<<DHT11_INPUTPIN); //high
+	_delay_us(1);
+	DHT11_DDR &= ~(1<<DHT11_INPUTPIN); //input
+	_delay_us(39);
+	//--
+	
+	//check start condition 1 (low)
+	if((DHT11_PIN & (1<<DHT11_INPUTPIN))) {
+		return DHT11_ERROR;
+	}
+	_delay_us(80);
+	//check start condition 2 (high)
+	if(!(DHT11_PIN & (1<<DHT11_INPUTPIN))) {
+		return DHT11_ERROR;
+	}
+	_delay_us(80);
+	
+	//read the data
+	for (j=0; j<5; j++) { //read 5 byte
+		uint8_t result=0;
+		for(i=0; i<8; i++) {//read every bit
+			while(!(DHT11_PIN & (1<<DHT11_INPUTPIN))); //wait for an high input
+			_delay_us(30);
+			if(DHT11_PIN & (1<<DHT11_INPUTPIN)) //if input is high after 30 us, get result
+			result |= (1<<(7-i));
+			while(DHT11_PIN & (1<<DHT11_INPUTPIN)); //wait until input get low
+		}
+		bits[j] = result;
+	}
+	
+	//reset port
+	DHT11_DDR |= (1<<DHT11_INPUTPIN); //output
+	DHT11_PORT |= (1<<DHT11_INPUTPIN); //low
+	_delay_ms(100);
+	
+	//check checksum
+	if (bits[0] + bits[1] + bits[2] + bits[3] == bits[4]) {
+		if (select == 0) { //return temperature
+			return(bits[2]);
+			} else if(select == 1){ //return humidity
+			return(bits[0]);
+		}
+	}
+	
+	return DHT11_ERROR;
+}
+
+void dht11_getdata(uint8_t num, uint8_t *data){
+	uint8_t buf = getdata(num);
+	if(buf == DHT11_ERROR){
+		;
+	}
+	else{
+		*data = buf;
+	}
+}
---
+++ software/UART-CS/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+
+namespace UART_CS
+{
+
+    public partial class Form1 : Form
+    {
+        class Account
+        {
+            public string tk { set; get; }
+            public string mk { set; get; }
+            public Account(string _tk, string _mk)
+            {
+                tk = _tk;
+                mk = _mk;
+            }
+        }
+        public Form1()
+        {
+            
+            InitializeComponent();
+            Init_Control();
+            LoadImage();
+            
+        }
+        public void Init_Control() 
+        {
+            //comboBoxBaud.Items.AddRange(SerialPort.GetPortNames().ToString()); 
+            comboBoxPort.Text = "";
+            comboBoxBaud.Text = "9600";
+            timer1.Start();
+
+        }
+        public void LoadImage()
+        {
+            try 
+            {
+                pictureBoxTime.Image = new Bitmap(Application.StartupPath + "\\Image\\gif\\original.gif");
+                pictureBox1.Image = new Bitmap(Application.StartupPath + "\\Image\\1.png");
+                pictureBoxDC.Image = new Bitmap(Application.StartupPath + "\\Image\\dongco\\a.jpg");
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBoxTime.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBoxDC.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            catch (Exception) { MessageBox.Show("Bug in picture"); }
+            
+        }
+        private void Login_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+                configurationBuilder.AddJsonFile(Application.StartupPath + "\\login.json");
+                IConfigurationRoot configurationRoot = configurationBuilder.Build();
+
+
+                if (AccountBox.Text == configurationRoot.GetSection("tk").Value && PassWordBox.Text == configurationRoot.GetSection("mk").Value)
+                    MessageBox.Show("Đăng nhập thành công");
+                else MessageBox.Show("Đăng Nhập Thất Bại", "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception) 
+            {
+                MessageBox.Show("Đăng Nhập Thất Bại", "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void PassWordBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Login_Click(sender, e);
+            }
+        }
+
+        private void AccountBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Login_Click(sender, e);
+            }
+        }
+        public int Led = 1;
+        private void buttonLed_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                Led = (Led < 2) ? 2 : 1;
+                buttonLed.Text = (buttonLed.Text == "Đóng") ? "Mở" : "Đóng";
+                if (buttonLed.Text == "Mở")
+                {
+                    serialPort.Write("1");
+                }
+                else if (buttonLed.Text == "Đóng")
+                {
+                    serialPort.Write("2");
+                }
+                pictureBox1.Image = new Bitmap(Application.StartupPath + "\\Image\\"+Led.ToString()+".png");
+            }
+            else MessageBox.Show("Vui long ket noi", "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            buttonConnect.Text = (buttonConnect.Text == "Connect") ? "Disconnect" : "Connect";
+            if (buttonConnect.Text == "Disconnect") 
+            {
+                serialPort.PortName = comboBoxPort.Text;
+                serialPort.BaudRate = Convert.ToInt32(comboBoxBaud.Text);
+            }
+            
+            try
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Close();
+                }
+                else
+                {
+                    serialPort.Open();
+                }
+            }
+            catch(Exception) 
+            {
+                MessageBox.Show("Vui lòng mở cổng COM", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+                
+        }
+
+        private void buttonUart_Click(object sender, EventArgs e)
+        {
+	        if(serialPort.IsOpen)
+		    {
+			    serialPort.Write(textBox1.Text);
+		    }
+            else
+		    {
+			    MessageBox.Show("Vui Lòng Mở Cổng COM");
+		    }
+        }
+        string kq = "";
+
+        private void SignUp_MouseClick(object sender, MouseEventArgs e)
+        {
+            Form f = new Form();
+            Button button = new Button() { Location = new Point(45, 181), Text = "OK" };
+            List<Label> label = new List<Label>
+            {
+                new Label { Location = new Point(45, 21),Text = "TaiKhoan" },
+                new Label { Location = new Point(45, 51), Text = "matkhau" },
+                new Label {Location = new Point(45, 81), Text = "Nhaplaimk" }
+            };
+            List<TextBox> textBoxes = new List<TextBox>
+            {
+                new TextBox{Location = new Point(120, 21)},
+                new TextBox{Location = new Point(120, 51)},
+                new TextBox{Location = new Point(120, 81)}
+            };
+            f.Controls.AddRange(textBoxes.ToArray());
+            f.Controls.Add(button);
+            foreach (var item in label)
+            {
+                f.Controls.Add(item);
+            }
+            void button_Click(object sender1, EventArgs e1)
+            {
+                if (textBoxes[1].Text == textBoxes[2].Text)
+                {
+                    Account account = new Account(textBoxes[0].Text.ToString(), textBoxes[1].Text.ToString());
+                    string json = JsonConvert.SerializeObject(account);
+                    var s = Application.StartupPath + "\\login.json";
+                    System.IO.File.WriteAllText(s, json);
+                    MessageBox.Show("Tao tai khoan thanh cong");
+                    f.Close();
+                }
+                else MessageBox.Show("Vui long nhap lai mat khau", "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            button.Click += button_Click;
+            f.ShowDialog();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelTime.Text = DateTime.Now.ToString("hh:mm:ss");
+        }
+        
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var dataPort = serialPort.ReadExisting();
+            kq += dataPort;
+            if (kq.Length >3)
+            {
+                //Invoke(new MethodInvoker(() => buttonLed_Click(sender, e)));
+                string kq1 = kq.ToString();
+                var compare = Convert.ToInt32(kq1.Substring(0,2));
+                compare = (compare == 0) ? 0 : (compare > 0 && compare <= 10) ? 10 : (compare > 10 && compare <= 30) ? 30 : (compare > 30 && compare <= 40) ? 40 : (compare > 40 && compare <= 44) ? 44 : (compare > 44 && compare <= 55) ? 55 : (compare > 55 && compare <= 60) ? 60 : (compare > 60 && compare < 70) ? 70 : 100;
+                pictureBoxNhietdo.Image = new Bitmap(Application.StartupPath + "\\Image\\nhietdo\\"+compare.ToString()+".jpg");
+                pictureBoxNhietdo.SizeMode = PictureBoxSizeMode.Zoom;
+                Invoke(new MethodInvoker(() => textBoxNhietdo.Text = kq1.ToString().Substring(0,2) + "oC"));
+                Invoke(new MethodInvoker(() => textBoxHumi.Text = kq1.ToString().Substring(2) + "oC"));
+                kq = "";
+            }
+        }
+        public string DC = "a";
+        private void buttonStartDC_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                DC = (DC == "a") ? "b" : "a";
+                buttonStartDC.Text = (buttonStartDC.Text == "StartDC") ? "StopDC" : "StartDC";
+                if (buttonStartDC.Text == "StartDC")
+                {
+                    serialPort.Write("a");
+                }
+                else if (buttonLed.Text == "StopDC")
+                {
+                    serialPort.Write("b");
+                }
+                pictureBoxDC.Image = new Bitmap(Application.StartupPath + "\\Image\\dongco\\" + DC + ".jpg");
+            }
+            else MessageBox.Show("Vui long ket noi", "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void comboBoxPort_DropDown(object sender, EventArgs e)
+        {
+            comboBoxPort.Items.Clear();
+            comboBoxPort.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames()); 
+        }
+    }
+}
